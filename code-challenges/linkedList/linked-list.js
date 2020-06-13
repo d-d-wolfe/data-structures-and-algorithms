fc901415d0526e8693f19ba3957b0af5e44ade06
d-d-wolfe <dave.wolfe623@gmail.com>
2020-06-12 22:50:05 -0700

    created the linked list.

---
+++ code-challenges/linkedList/linked-list.js
@@ -0,0 +1,49 @@
+'use strict';
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  traverse() {
+    let current = this.head;
+
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+
+  insert() {
+
+  }
+
+  toString() {
+
+  }
+
+  includes() {
+
+  }
+}
+
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+const linkedList = new LinkedList();
+const firstNode = new Node('some-info');
+const secondNode = new Node('second-info');
+const thirdNode = new Node('third-info');
+
+linkedList.head = firstNode;
+linkedList.head.next = secondNode;
+linkedList.head.next.next = thirdNode;
+
+linkedList.traverse();
+
+
