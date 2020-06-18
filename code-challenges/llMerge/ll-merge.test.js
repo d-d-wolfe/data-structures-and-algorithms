099b757034ab773f27199b405e3131d5935fb599
d-d-wolfe <dave.wolfe623@gmail.com>
2020-06-17 18:42:21 -0700

    still working on this

---
+++ code-challenges/llMerge/ll-merge.test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const linkedList = require('./ll-merge.js');
+
+const firstLinkedList = new linkedList.LinkedList();
+const secondLinkedList = new linkedList.LinkedList();
+const mergeList = new linkedList.LinkedList();
+
+firstLinkedList.insert('2');
+firstLinkedList.insert('3');
+firstLinkedList.insert('1');
+
+secondLinkedList.insert('4');
+secondLinkedList.insert('9');
+secondLinkedList.insert('5');
+
+
+describe('Testing Merged Linked Lists', () => {
+  it('Should zipper two linked lists together by taking in 3 linked list variables', () => {
+    // console.log(linkedList.mergeLists());
+    expect(linkedList.mergeLists(firstLinkedList, secondLinkedList, mergeList)).toStrictEqual(` { 4 } { 2 } { 9 } { 3 } { 5 } { 1 }`);
+  });
+});
+
