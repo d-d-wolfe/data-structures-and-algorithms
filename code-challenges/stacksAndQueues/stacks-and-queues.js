6f65079398f837aa64619dde3b883950cdf52414
d-d-wolfe <dave.wolfe623@gmail.com>
2020-06-20 10:57:41 -0700

    completed the queue section

--- code-challenges/stacksAndQueues/stacks-and-queues.js
+++ code-challenges/stacksAndQueues/stacks-and-queues.js
@@ -25,7 +25,7 @@ class Stack {
 
   pop() {
     if (this.top === null) {
-      return ('The stack is empty, nothing to remove.')
+      return ('The stack is empty, nothing to remove.');
     }
     let value = this.Top;
     this.Top = this.Top.next;
@@ -33,8 +33,23 @@ class Stack {
     return value;
   }
 
+  onlyValues() {
+    let current = this.top;
+
+    let string = '';
+    while (current) {
+      string += ` { ${current.value} } `;
+      current = current.next;
+    }
+    return string;
+  }
+
   isEmpty() {
     return (this.top === null)
+  }
+
+  isEmpty() {
+    return (this.top === null);
 
   }
 }
@@ -53,9 +68,56 @@ console.log(stack.peek());
 console.log(stack.isEmpty());
 
 
+class Queue {
+  constructor() {
+    this.front = null;
+    this.rear = null;
+  }
+
+  enqueue(value) {
+    let newNode = new Node(value);
+    if (this.rear) {
+      this.rear.next = newNode;
+    } else {
+      this.front = newNode;
+    }
+    this.rear = newNode;
+  }
+
+  dequeue() {
+    let removed = this.front.value;
+    this.front = this.front.next;
+    return removed;
+  }
+
+
+  peek() {
+    return this.front.value;
+  }
+
+  isEmpty() {
+    return (this.front === null);
+  }
+}
+
+let queue = new Queue;
+
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+queue.dequeue();
+
+console.log(queue);
+
+console.log(queue.isEmpty());
+console.log(queue.peek());
+
+
+
 
 
 module.exports = {
   stack: Stack,
-  // queue: Queue,
+  queue: Queue,
 };
--- code-challenges/stacksAndQueues/stacks-and-queues.test.js
+++ code-challenges/stacksAndQueues/stacks-and-queues.test.js
@@ -2,3 +2,14 @@
 
 let stacks = require('./stacks-and-queues.js');
 
+
+describe('Tests to verify push to stack works', () => {
+  it('should push nodes to the stack', () => {
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+
+    expect()
+  })
+})
