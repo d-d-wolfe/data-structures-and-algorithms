a5314e82579be06a6b5e3ba0e250479a4e723c0b
d-d-wolfe <dave.wolfe623@gmail.com>
2020-07-02 19:06:41 -0700

    completed fizzbuzz challenge

--- code-challenges/fizzBuzzTree/fizz-buzz-tree.js
+++ code-challenges/fizzBuzzTree/fizz-buzz-tree.js
@@ -13,21 +13,53 @@ class Ktree {
   }
 
   preOrder(root) {
-    console.log(root.value);
+    let x =[];
+    x.push(root.value);
 
     for (let i = 0; i < root.child.length; i++) {
       if (root.child[i] !== null) {
-        this.preOrder(root.child[i]);
+        x.push(this.preOrder(root.child[i]));
       }
     }
+    return x;
+  }
+
+  fizzBuzz(root) {
+    let newTree = new Ktree();
+
+    if (root.value % 3 === 0) {
+      newTree = new Node('fizz');
+    }
+    if (root.value % 5 === 0) {
+      newTree = new Node('buzz');
+    }
+    if (root.value % 5 === 0 && root.value % 3 === 0) {
+      newTree = new Node('FizzBuzz');
+    }
+    if (root.value % 3 !== 0 && root.value % 5 !== 0) {
+      newTree = new Node(`${root.value}`);
+    }
+    for (let i = 0; i < root.child.length; i++) {
+      if (root.child[i] !== null) {
+        newTree.child[i] = this.fizzBuzz(root.child[i], newTree.child[i]);
+      }
+    }
+    return newTree;
   }
 }
 
 let newTree = new Ktree();
 newTree.root = new Node(1);
-newTree.root.child[0] = new Node(2);
+newTree.root.child[0] = new Node(15);
 newTree.root.child[1] = new Node(3);
-// newTree.root.child.child[0] = new Node(4);
-// newTree.root.child.child[1] = new Node(5);
+newTree.root.child[0].child[0] = new Node(4);
+newTree.root.child[1].child[1] = new Node(5);
+newTree.root.child[0].child[1] = new Node(6);
+newTree.root.child[1].child[0] = new Node(7);
 
-console.log(newTree);
+console.log(newTree.preOrder(newTree.fizzBuzz(newTree.root)));
+
+module.exports = {
+  Node,
+  Ktree
+};
